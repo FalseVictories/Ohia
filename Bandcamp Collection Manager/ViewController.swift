b6a3d6eb0a344a314b42d9a3ef30fa146d3a1527
iain <iain@falsevictories.com>
2023-10-02 11:28:45 +0100

    Initial Commit

---
+++ Bandcamp Collection Manager/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Bandcamp Collection Manager
+//
+//  Created by iain on 02/10/2023.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
