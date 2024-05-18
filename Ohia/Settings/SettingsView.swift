ea682b695d8a4b72190298483d780951986a274b
iain <iain@falsevictories.com>
2024-05-18 13:01:37 +0100

    Improve the settings

--- Ohia/Settings/SettingsView.swift
+++ Ohia/Settings/SettingsView.swift
@@ -13,43 +13,65 @@ struct SettingsView: View {
     
     var body: some View {
         Form {
-            LabeledContent("Download Folder:") {
-                FilePickerButton(folder: $settingsModel.selectedDownloadFolder)
+            Section {
+                LabeledContent("Download Folder:") {
+                    FilePickerButton(folder: $settingsModel.selectedDownloadFolder)
+                }
+                
+                Picker(selection: $settingsModel.selectedFileFormat, content: {
+                    Text("Flac").tag(FileFormat.flac)
+                    Text("MP3 v0").tag(FileFormat.mp3_v0)
+                    Text("MP3 320").tag(FileFormat.mp3_320)
+                    Text("Wav").tag(FileFormat.wav)
+                    Text("AAC High").tag(FileFormat.aachi)
+                    Text("Ogg Vorbis").tag(FileFormat.vorbis)
+                    Text("ALAC").tag(FileFormat.alac)
+                    Text("AIFF Lossless").tag(FileFormat.aiff)
+                    
+                }, label: {
+                    Text("Download Format")
+                })
+                
+                // Limit to 30 downloads at once? Dunno. Seems high enough to not annoy anyone.
+                
+                LabeledContent(content: {
+                    Text("\(settingsModel.maxDownloads)")
+                    Stepper(value: $settingsModel.maxDownloads,
+                            label: {})
+                }, label: {
+                    Text("Maximum concurrent downloads")
+                })
+                
+                Toggle(isOn: $settingsModel.downloadPreorders, label: {
+                    Text("Download pre-orders")
+                })
+                
+            } header: {
             }
             
-            Picker("Download Format:", selection: $settingsModel.selectedFileFormat) {
-                Text("Flac").tag(FileFormat.flac)
-                Text("MP3 v0").tag(FileFormat.mp3_v0)
-                Text("MP3 320").tag(FileFormat.mp3_320)
-                Text("Wav").tag(FileFormat.wav)
-                Text("AAC High").tag(FileFormat.aachi)
-                Text("Ogg Vorbis").tag(FileFormat.vorbis)
-                Text("ALAC").tag(FileFormat.alac)
-                Text("AIFF Lossless").tag(FileFormat.aiff)
+            Section {
+                Toggle(isOn: $settingsModel.overwrite, label: {
+                    Text("Overwrite existing downloads")
+                })
+                
+                Toggle(isOn: $settingsModel.decompressDownloads, label: {
+                    Text("Decompress downloads")
+                })
+                
+                Picker(selection: $settingsModel.createFolderStructure, content: {
+                    Text("None").tag(FolderStructure.none)
+                    Text("Artist - Title /").tag(FolderStructure.single)
+                    Text("Artist / Title /").tag(FolderStructure.multi)
+                    Text("Artist / Artist - Title /").tag(FolderStructure.bandcamp)
+                    
+                }, label: {
+                    Text("Create Folders:")
+                })
+                .disabled(!settingsModel.decompressDownloads)
+            } header: {
             }
-
-            Spacer()
-                .frame(height: 20)
-            
-            // Limit to 30 downloads at once? Dunno. Seems high enough to not annoy anyone.
-            TextField("Max concurrent downloads:",
-                      value: $settingsModel.maxDownloads,
-                      format: .ranged(1...30))
-            Toggle("Download pre-orders", isOn: $settingsModel.downloadPreorders)
-            
-            Spacer()
-                .frame(height: 20)
-            
-            Toggle("Overwrite existing downloads", isOn: $settingsModel.overwrite)
-            
-            Toggle("Decompress downloads", isOn: $settingsModel.decompressDownloads)
-            Picker("Create Folders:", selection: $settingsModel.createFolderStructure) {
-                Text("None").tag(FolderStructure.none)
-                Text("Artist - Title /").tag(FolderStructure.single)
-                Text("Artist / Title /").tag(FolderStructure.multi)
-                Text("Artist / Artist - Title /").tag(FolderStructure.bandcamp)
-            }.disabled(!settingsModel.decompressDownloads)
         }
+        .formStyle(.grouped)
         .padding()
     }
 }
